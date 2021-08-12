00c39549310a1067dbd3cc161971108be73ed46a
aiyboy <59334928+aiyboy@users.noreply.github.com>
2021-08-12 19:10:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,7 +13,7 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 #
-#git clone https://github.com/fw876/helloworld package/helloworld
-#git clone https://github.com/kenzok8/small package/small
-#git clone https://github.com/kenzok8/openwrt-packages package/kenzo
+git clone https://github.com/fw876/helloworld package/helloworld
+git clone https://github.com/kenzok8/small package/small
+git clone https://github.com/kenzok8/openwrt-packages package/kenzo
 
